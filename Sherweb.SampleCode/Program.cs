bbd68580496ba0bb3cf49406dc1acb3dba739c6f
Sébastien Pons <18288950+CobraBoy@users.noreply.github.com>
2020-09-17 11:48:33 -0400

    Add SampleCode project

---
+++ Sherweb.SampleCode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Rest;
+using Sherweb.Apis.Authorization;
+using Sherweb.Apis.Distribution;
+
+namespace Sherweb.SampleCode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Get Bearer Token from Authorization API
+            var authorizationClient = new AuthorizationService(new Uri("https://api-testing.sherweb.com/auth"));
+
+            var token = authorizationClient.TokenMethod(
+                "5d194860-62e5-405e-85a2-3c2d451e4a3a", // ClientId
+                "qXsCDWHNzXhrUS3I/u4opco7DBgT8fiMaceIzKb7", // ClientSecret
+                "distributor"); // Scope : distributor for Distribution API
+
+            //// Instantiate Distribution API client using Bearer token
+            var svcClientCreds = new TokenCredentials(token.AccessToken, "Bearer");
+            var distributionClient = new DistributionService(
+                new Uri("https://api-testing.sherweb.com/distribution/v1"),
+                svcClientCreds,
+                new SubscriptionKeyHandler());
+
+            var accountPayable = distributionClient.GetAccountsPayable();
+        }
+    }
+}
---
+++ Sherweb.SampleCode/SubscriptionKeyHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sherweb.SampleCode
+{
+    public class SubscriptionKeyHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            request.Headers.Add("Ocp-Apim-Subscription-Key", "fab21255feb049b2aabcd80b3f584885");
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
