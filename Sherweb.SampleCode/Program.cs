a112376b752d3d92b0a6d17977c88f3cae608cd3
Alexandre Leclerc <aleclerc@sherweb.com>
2020-11-23 10:38:52 -0500

    Write in console the problemDetails in the sample code if an error occured. Add a comment to indicate to write the subscriptionKey in SubscriptionKeyHandler.cs

--- Sherweb.SampleCode/Program.cs
+++ Sherweb.SampleCode/Program.cs
@@ -27,12 +27,28 @@ namespace Sherweb.SampleCode
             var distributorClient = new DistributorService(
                 new Uri("https://api.sherweb.com/distributor/v1"),
                 svcClientCreds,
-                new SubscriptionKeyHandler());
+                new SubscriptionKeyHandler()); // Add your subscription key in the SubscriptionKey.cs file
 
             var response = distributorClient.GetPayableCharges();
             if (response is ProblemDetails problemDetails)
             {
-                // Handle error
+                Console.WriteLine($"{nameof(problemDetails.Instance)}={problemDetails.Instance}");
+                Console.WriteLine($"{nameof(problemDetails.Title)}={problemDetails.Title}");
+                Console.WriteLine($"{nameof(problemDetails.Status)}={problemDetails.Status}");
+                Console.WriteLine($"{nameof(problemDetails.Type)}={problemDetails.Type}");
+                Console.WriteLine($"{nameof(problemDetails.Detail)}={problemDetails.Detail}");
+
+                if (problemDetails.Extensions != null)
+                {
+                    Console.WriteLine($"{nameof(problemDetails.Detail)}={problemDetails.Detail}");
+                    foreach (var extension in problemDetails.Extensions)
+                    {
+                        Console.WriteLine($"{nameof(extension.Key)}={extension.Key}");
+                        Console.WriteLine($"{nameof(extension.Value)}={extension.Value}");
+                        Console.WriteLine("-------------------------------------------------");
+                    }
+                }
+
                 return;
             }
 
