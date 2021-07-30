a48a1ff79f59260d36efea191cdfae7aa72d92ed
Joy Korji <47510906+joykorji@users.noreply.github.com>
2021-07-30 10:45:20 -0400

    Update ServiceProviderServiceExtensions.cs

--- NugetPackagesSourceCode/Sherweb.Apis.ServiceProvider/HttpClient/ServiceProviderServiceExtensions.cs
+++ NugetPackagesSourceCode/Sherweb.Apis.ServiceProvider/HttpClient/ServiceProviderServiceExtensions.cs
@@ -11,122 +11,121 @@ namespace Sherweb.Apis.ServiceProvider
     using System.Threading.Tasks;
 
     /// <summary>
-    /// Extension methods for ServiceProvidingPublicApiClient.
+    /// Extension methods for ServiceProviderService.
     /// </summary>
-    public static partial class ServiceProvidingPublicApiClientExtensions
+    public static partial class ServiceProviderServiceExtensions
     {
-            /// <summary>
-            /// GetCustomers (beta)
-            /// </summary>
-            /// <remarks>
-            /// Get the list of all your customers.
-            /// </remarks>
-            /// <param name='operations'>
-            /// The operations group for this extension method.
-            /// </param>
-            /// <param name='serviceProviderId'>
-            /// </param>
-            /// <param name='acceptLanguage'>
-            /// Specify language (and culture) following [RFC 7231, section 5.3.5:
-            /// Accept-Language].
-            /// </param>
-            public static Customers GetCustomers(this IServiceProvidingPublicApiClient operations, System.Guid serviceProviderId, string acceptLanguage = default(string))
-            {
-                return operations.GetCustomersAsync(serviceProviderId, acceptLanguage).GetAwaiter().GetResult();
-            }
+        /// <summary>
+        /// GetCustomers (beta)
+        /// </summary>
+        /// <remarks>
+        /// Get the list of all your customers.
+        /// </remarks>
+        /// <param name='operations'>
+        /// The operations group for this extension method.
+        /// </param>
+        /// <param name='serviceProviderId'>
+        /// </param>
+        /// <param name='acceptLanguage'>
+        /// Specify language (and culture) following [RFC 7231, section 5.3.5:
+        /// Accept-Language].
+        /// </param>
+        public static Customers GetCustomers(this IServiceProviderService operations, System.Guid serviceProviderId, string acceptLanguage = default(string))
+        {
+            return operations.GetCustomersAsync(serviceProviderId, acceptLanguage).GetAwaiter().GetResult();
+        }
 
-            /// <summary>
-            /// GetCustomers (beta)
-            /// </summary>
-            /// <remarks>
-            /// Get the list of all your customers.
-            /// </remarks>
-            /// <param name='operations'>
-            /// The operations group for this extension method.
-            /// </param>
-            /// <param name='serviceProviderId'>
-            /// </param>
-            /// <param name='acceptLanguage'>
-            /// Specify language (and culture) following [RFC 7231, section 5.3.5:
-            /// Accept-Language].
-            /// </param>
-            /// <param name='cancellationToken'>
-            /// The cancellation token.
-            /// </param>
-            public static async Task<Customers> GetCustomersAsync(this IServiceProvidingPublicApiClient operations, System.Guid serviceProviderId, string acceptLanguage = default(string), CancellationToken cancellationToken = default(CancellationToken))
+        /// <summary>
+        /// GetCustomers (beta)
+        /// </summary>
+        /// <remarks>
+        /// Get the list of all your customers.
+        /// </remarks>
+        /// <param name='operations'>
+        /// The operations group for this extension method.
+        /// </param>
+        /// <param name='serviceProviderId'>
+        /// </param>
+        /// <param name='acceptLanguage'>
+        /// Specify language (and culture) following [RFC 7231, section 5.3.5:
+        /// Accept-Language].
+        /// </param>
+        /// <param name='cancellationToken'>
+        /// The cancellation token.
+        /// </param>
+        public static async Task<Customers> GetCustomersAsync(this IServiceProviderService operations, System.Guid serviceProviderId, string acceptLanguage = default(string), CancellationToken cancellationToken = default(CancellationToken))
+        {
+            using (var _result = await operations.GetCustomersWithHttpMessagesAsync(serviceProviderId, acceptLanguage, null, cancellationToken).ConfigureAwait(false))
             {
-                using (var _result = await operations.GetCustomersWithHttpMessagesAsync(serviceProviderId, acceptLanguage, null, cancellationToken).ConfigureAwait(false))
-                {
-                    return _result.Body;
-                }
+                return _result.Body;
             }
+        }
 
-            /// <summary>
-            /// GetReceivableCharges (beta)
-            /// </summary>
-            /// <remarks>
-            /// Get the amounts that one of your customers owe you for a specific billing
-            /// period. There are three types of charges that are included in the data
-            /// returned: recurring, usage, and setup.
-            /// </remarks>
-            /// <param name='operations'>
-            /// The operations group for this extension method.
-            /// </param>
-            /// <param name='serviceProviderId'>
-            /// </param>
-            /// <param name='customerId'>
-            /// </param>
-            /// <param name='date'>
-            /// Specify a date within the desired billing period. Format: yyyy-MM-dd (UTC).
-            /// Default: Today. For example, if the date is March 17th and your billing
-            /// period is from the 1st to the 31st of the month, it will return data from
-            /// March 1st to March 31st.
-            /// </param>
-            /// <param name='acceptLanguage'>
-            /// Specify language (and culture) following [RFC 7231, section 5.3.5:
-            /// Accept-Language].
-            /// </param>
-            public static ReceivableCharges GetReceivableCharges(this IServiceProvidingPublicApiClient operations, System.Guid serviceProviderId, System.Guid customerId, System.DateTime? date = default(System.DateTime?), string acceptLanguage = default(string))
-            {
-                return operations.GetReceivableChargesAsync(serviceProviderId, customerId, date, acceptLanguage).GetAwaiter().GetResult();
-            }
+        /// <summary>
+        /// GetReceivableCharges (beta)
+        /// </summary>
+        /// <remarks>
+        /// Get the amounts that one of your customers owe you for a specific billing
+        /// period. There are three types of charges that are included in the data
+        /// returned: recurring, usage, and setup.
+        /// </remarks>
+        /// <param name='operations'>
+        /// The operations group for this extension method.
+        /// </param>
+        /// <param name='serviceProviderId'>
+        /// </param>
+        /// <param name='customerId'>
+        /// </param>
+        /// <param name='date'>
+        /// Specify a date within the desired billing period. Format: yyyy-MM-dd (UTC).
+        /// Default: Today. For example, if the date is March 17th and your billing
+        /// period is from the 1st to the 31st of the month, it will return data from
+        /// March 1st to March 31st.
+        /// </param>
+        /// <param name='acceptLanguage'>
+        /// Specify language (and culture) following [RFC 7231, section 5.3.5:
+        /// Accept-Language].
+        /// </param>
+        public static ReceivableCharges GetReceivableCharges(this IServiceProviderService operations, System.Guid serviceProviderId, System.Guid customerId, System.DateTime? date = default(System.DateTime?), string acceptLanguage = default(string))
+        {
+            return operations.GetReceivableChargesAsync(serviceProviderId, customerId, date, acceptLanguage).GetAwaiter().GetResult();
+        }
 
-            /// <summary>
-            /// GetReceivableCharges (beta)
-            /// </summary>
-            /// <remarks>
-            /// Get the amounts that one of your customers owe you for a specific billing
-            /// period. There are three types of charges that are included in the data
-            /// returned: recurring, usage, and setup.
-            /// </remarks>
-            /// <param name='operations'>
-            /// The operations group for this extension method.
-            /// </param>
-            /// <param name='serviceProviderId'>
-            /// </param>
-            /// <param name='customerId'>
-            /// </param>
-            /// <param name='date'>
-            /// Specify a date within the desired billing period. Format: yyyy-MM-dd (UTC).
-            /// Default: Today. For example, if the date is March 17th and your billing
-            /// period is from the 1st to the 31st of the month, it will return data from
-            /// March 1st to March 31st.
-            /// </param>
-            /// <param name='acceptLanguage'>
-            /// Specify language (and culture) following [RFC 7231, section 5.3.5:
-            /// Accept-Language].
-            /// </param>
-            /// <param name='cancellationToken'>
-            /// The cancellation token.
-            /// </param>
-            public static async Task<ReceivableCharges> GetReceivableChargesAsync(this IServiceProvidingPublicApiClient operations, System.Guid serviceProviderId, System.Guid customerId, System.DateTime? date = default(System.DateTime?), string acceptLanguage = default(string), CancellationToken cancellationToken = default(CancellationToken))
+        /// <summary>
+        /// GetReceivableCharges (beta)
+        /// </summary>
+        /// <remarks>
+        /// Get the amounts that one of your customers owe you for a specific billing
+        /// period. There are three types of charges that are included in the data
+        /// returned: recurring, usage, and setup.
+        /// </remarks>
+        /// <param name='operations'>
+        /// The operations group for this extension method.
+        /// </param>
+        /// <param name='serviceProviderId'>
+        /// </param>
+        /// <param name='customerId'>
+        /// </param>
+        /// <param name='date'>
+        /// Specify a date within the desired billing period. Format: yyyy-MM-dd (UTC).
+        /// Default: Today. For example, if the date is March 17th and your billing
+        /// period is from the 1st to the 31st of the month, it will return data from
+        /// March 1st to March 31st.
+        /// </param>
+        /// <param name='acceptLanguage'>
+        /// Specify language (and culture) following [RFC 7231, section 5.3.5:
+        /// Accept-Language].
+        /// </param>
+        /// <param name='cancellationToken'>
+        /// The cancellation token.
+        /// </param>
+        public static async Task<ReceivableCharges> GetReceivableChargesAsync(this IServiceProviderService operations, System.Guid serviceProviderId, System.Guid customerId, System.DateTime? date = default(System.DateTime?), string acceptLanguage = default(string), CancellationToken cancellationToken = default(CancellationToken))
+        {
+            using (var _result = await operations.GetReceivableChargesWithHttpMessagesAsync(serviceProviderId, customerId, date, acceptLanguage, null, cancellationToken).ConfigureAwait(false))
             {
-                using (var _result = await operations.GetReceivableChargesWithHttpMessagesAsync(serviceProviderId, customerId, date, acceptLanguage, null, cancellationToken).ConfigureAwait(false))
-                {
-                    return _result.Body;
-                }
+                return _result.Body;
             }
+        }
 
     }
 }
-
