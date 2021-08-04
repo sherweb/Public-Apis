9336a4b0375383cbead2e0f7c3e161c895d3e1ff
Nikita Yash Patel <nypatel@dns1.sherweb.com>
2021-08-04 11:45:57 -0400

    Added menu for sample code

--- SampleCode/Program.cs
+++ SampleCode/Program.cs
@@ -1,10 +1,15 @@
+#region
+
 using System;
 using Microsoft.Rest;
+
 using Sherweb.Apis.Authorization;
 using Sherweb.Apis.Distributor;
 using Sherweb.Apis.Distributor.Factory;
 using Sherweb.Apis.ServiceProvider;
 using Sherweb.Apis.ServiceProvider.Factory;
+
+#endregion
 
 namespace Sherweb.SampleCode
 {
@@ -22,20 +27,14 @@ namespace Sherweb.SampleCode
 
         private static AuthorizationService _authorizationClient;
 
-        private static IDistributorService _distributorClient;
+        private static int _selectedMenuOptions;
 
-        private static IServiceProviderService _serviceProviderClient;
-
-        private static SubscriptionService _subscriptionService;
-
-        private static CustomerService _customerService;
-
-        private static DistributionService _distributionService;
+        private static readonly MainMenu _menu = new MainMenu();
 
         static void Main(string[] args)
         {
             _baseUrl = "https://api.sherweb.com";
-            
+
             _clientId = "your client id";
             _clientSecret = "your client secret";
             _subscriptionKey = "your subscription key";
@@ -43,15 +42,14 @@ namespace Sherweb.SampleCode
             // Optional. This should follow [RFC 7231, section 5.3.5: Accept-Language]: https://tools.ietf.org/html/rfc7231#section-5.3.5
             // Example: en, en-CA;q=0.8, fr-CA;q=0.7
             _acceptLanguageHeader = null;
-
             // Get Bearer Token from Authorization API
             _authorizationClient = new AuthorizationService(new Uri($"{_baseUrl}/auth"));
-
-            _distributorClient = BuildDistributorClient();
-            _serviceProviderClient = BuildServiceProviderClient();
-            _subscriptionService = new SubscriptionService(_serviceProviderClient);
-            _customerService = new CustomerService(_serviceProviderClient);
-            _distributionService = new DistributionService(_distributorClient);
+            Console.Clear();
+            while (true)
+            {
+                _menu.PrintMenu(new MainMenu.MenuOptions());
+                ProcessMainMenu(Console.ReadLine());
+            }
         }
 
         private static IDistributorService BuildDistributorClient()
@@ -74,7 +72,8 @@ namespace Sherweb.SampleCode
                 RetryCount = 0
             };
 
-            var distributorServiceFactory = new DistributorServiceFactory(distributorConfiguration,
+            var distributorServiceFactory = new DistributorServiceFactory(
+                distributorConfiguration,
                 new SubscriptionKeyHandler(_subscriptionKey));
 
             return distributorServiceFactory.Create();
@@ -104,6 +103,129 @@ namespace Sherweb.SampleCode
                 new SubscriptionKeyHandler(_subscriptionKey));
 
             return serviceProviderServiceFactory.Create();
+        }
+
+        public static void ProcessMainMenu(string input)
+        {
+            if (int.TryParse(input, out _selectedMenuOptions))
+            {
+                switch (_selectedMenuOptions)
+                {
+                    case (int)MainMenu.MenuOptions.Distributor:
+                        {
+                            ProcessDistribution();
+                            break;
+                        }
+                    case (int)MainMenu.MenuOptions.ServiceProvider:
+                        {
+                            ProcessServiceProvider();
+                            break;
+                        }
+                    case 0:
+                        {
+                            Environment.Exit(0);
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Try again!!");
+                            break;
+                        }
+                }
+            }
+        }
+
+        private static void ProcessDistribution()
+        {
+            var distributorClient = BuildDistributorClient();
+            var distributionService = new DistributionService(distributorClient);
+            while (true)
+            {
+                _menu.PrintMenu(new MainMenu.DistributorOption());
+                var input = Console.ReadLine();
+                if (int.TryParse(input, out _selectedMenuOptions))
+                {
+                    switch (_selectedMenuOptions)
+                    {
+                        case (int)MainMenu.DistributorOption.GetPayableCharges:
+                            {
+                                distributionService.ShowPayableCharges(_acceptLanguageHeader);
+                                break;
+                            }
+                        case 0:
+                            {
+                                return;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Try again!!");
+                                break;
+                            }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Try again!!");
+                }
+            }
+        }
+
+        private static void ProcessServiceProvider()
+        {
+            var serviceProviderClient = BuildServiceProviderClient();
+            var subscriptionService = new SubscriptionService(serviceProviderClient);
+
+            while (true)
+            {
+                _menu.PrintMenu(new MainMenu.ServiceProviderOption());
+                var input = Console.ReadLine();
+                if (int.TryParse(input, out _selectedMenuOptions))
+                {
+                    switch (_selectedMenuOptions)
+                    {
+                        case (int)MainMenu.ServiceProviderOption.GetCustomers:
+                            {
+                                subscriptionService.GetCustomers(_acceptLanguageHeader);
+                                break;
+                            }
+                        case (int)MainMenu.ServiceProviderOption.GetSubscriptions:
+                            {
+                                Console.WriteLine("Enter CustomerId :");
+                                var customerId = Console.ReadLine();
+                                if (!string.IsNullOrWhiteSpace(customerId))
+                                {
+                                    subscriptionService.GetSubscriptions(new Guid(customerId), _acceptLanguageHeader);
+                                }
+
+                                break;
+                            }
+                        case (int)MainMenu.ServiceProviderOption.GetSubscriptionsAmendmentStatus:
+                            {
+                                Console.WriteLine("Enter Subscription AmendmentId :");
+                                var subscriptionsAmendmentId = Console.ReadLine();
+                                if (!string.IsNullOrWhiteSpace(subscriptionsAmendmentId))
+                                {
+                                    subscriptionService.GetAmendmentStatus(new Guid(subscriptionsAmendmentId), _acceptLanguageHeader);
+                                }
+
+                                break;
+                            }
+                        case 0:
+                            {
+                                return;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Try again!!");
+                                break;
+                            }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Try again!!");
+                }
+            }
         }
     }
 }
