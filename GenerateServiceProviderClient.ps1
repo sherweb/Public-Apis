61a301f571e507a8f211ee9f6ebc64fd022893a2
Mathieu Fournier <mfournier@dns1.sherweb.com>
2021-08-12 10:01:54 -0400

    ServiceProvider: Bump version to beta. Add GenerateServiceProviderClient

---
+++ GenerateServiceProviderClient.ps1
@@ -0,0 +1,1 @@
+autorest --input-file="Sherweb.Apis.ServiceProvider.OpenAPI.json" --csharp --output-folder=Sherweb.Apis.ServiceProvider\HttpClient --namespace=Sherweb.Apis.ServiceProvider --override-client-name='ServiceProviderService' --add-credentials=true --v3
